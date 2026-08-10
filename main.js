6477a3c64a790a695e799564c2c764ff9414c0a0
clumsyBaboon <godnicnic@gmail.com>
2026-08-10 17:47:35 +0300

    auto 10.08.2026  17:47:35,68

--- main.js
+++ main.js
@@ -85,7 +85,7 @@ app.whenReady().then(() => {
     }
   })
 
-  try {
+  try { // ЧТЕНИЕ ТОКЕН ФАЙЛА
     const filePath = path.join(app.getPath("userData"), "token.txt"); // Путь к токен файлу
     if (fs.existsSync(filePath)) { // Проверка ли существует файл
         const data = fs.readFileSync(filePath, "utf-8"); // Чтение файла
@@ -96,40 +96,44 @@ app.whenReady().then(() => {
     print(`Error in reading token file: ${err}`, "err"); // Вывод ошибки в консоль
   }
 
-  try {
-    const filePath = path.join(app.getPath("userData"), "config.json");
-    if (fs.existsSync(filePath)) {
-        const data = fs.readFileSync(filePath, "utf-8");
-        config = JSON.parse(data);
-        print("Config file was successfully read");
+  try { // ЧТЕНИЕ КОНФИГ ФАЙЛА
+    const filePath = path.join(app.getPath("userData"), "config.json"); // Путь к конфиг файлу
+    if (fs.existsSync(filePath)) { // Проверка ли существует файл
+        const data = fs.readFileSync(filePath, "utf-8"); // Чтение файла
+        config = JSON.parse(data); // Сохранение в переменную конфига
+        print("Config file was successfully read"); // Вывод результат чтения
     } else print("Config file doesn't exist");
-  } catch (err) {
-    print(`Error in reading config file: ${err}`, "err");
+  } catch (err) { // Ошибка при чтении файла
+    print(`Error in reading config file: ${err}`, "err"); // Вывод ошибки в консоль
   }
-  print(config);
-  win.webContents.on("did-finish-load", () => {
-    win.webContents.send("config-file", config);
-    if (config.autoConnect) connectWS();
+  print(config); // Вывод текущего конфига в консоль
+  win.webContents.on("did-finish-load", () => { // Как загрузится окно electron
+    win.webContents.send("config-file", config); // Отправить конфиг в окно electron
+    if (config.autoConnect) connectWS(); // Автоматическое подключение к ютуб музыке (если включено в настройках)
   })
 })
 
-// Если все окна закрыты - закрыть сокет !Добавить мак ос
+// Если все окна закрыты -> закрыть сокет !Добавить мак ос
 app.on('window-all-closed', () => {
     if (socket != null) socket.disconnect();
     app.quit() // Закрыть программу
 })
 
+// ===== ФУНКЦИИ ИЗ ELECTRON =====
+
+// Запрос токена \ Первое подключение
 ipcMain.on("connect-api", async event => {
-    print("Start connecting API...");
-    //requestcode
-    const url1 = "http://localhost:9863/api/v1/auth/requestcode";
+    print("Start connecting API..."); // Вывод в консоль
+    // requestcode \ Первый запрос
+    const url1 = "http://localhost:9863/api/v1/auth/requestcode"; // Адрес запроса временного кода
     const data1 = {
         "appId": appId,
         "appName": pkg.name,
         "appVersion": VERSION
     }
-    let CODE;
+    let CODE; // Временный код для второго запроса
     try {
+        // POST запрос
         const response = await fetch(url1, {
             method: "POST",
             headers: {
@@ -138,28 +142,28 @@ ipcMain.on("connect-api", async event => {
             body: JSON.stringify(data1)
         })
 
-        if (!response.ok) {
-            const errText = await response.text();
+        if (!response.ok) { // Если ошибка
+            const errText = await response.text(); // Текст ошибки
             throw new Error(`Status: ${response.status} - ${errText}`);
         }
 
-        const result = await response.json();
-        CODE = await result.code;
-    } catch (err) {
-        print(`Error in fetch: ${err}`, "err");
-        return;
+        const result = await response.json(); // Результат запроса
+        CODE = await result.code; // Достать временный код из результата
+    } catch (err) { // Если ошибка
+        print(`Error in fetch: ${err}`, "err"); // Вывод в консоль
+        return; // Выйти досрочно из ф-ции
     }
-    print(`CODE: ${CODE}`);
+    print(`CODE: ${CODE}`); // Вывод временного кода в консоль
 
-    //request
-    const url2 = "http://localhost:9863/api/v1/auth/request";
+    // requestToken \ Второй запрос
+    const url2 = "http://localhost:9863/api/v1/auth/request"; // Адрес запроса токена
     const data2 = {
         "appId": appId,
         "appName": pkg.name,
         "appVersion": VERSION,
         "code": CODE
     }
-    let TOKEN_buffer;
+    let TOKEN_buffer; // Временная переменная для хранения токена
     try {
         const response = await fetch(url2, {
             method: "POST",
@@ -169,27 +173,29 @@ ipcMain.on("connect-api", async event => {
             body: JSON.stringify(data2)
         })
 
-        if (!response.ok) {
-            const errText = await response.text();
+        if (!response.ok) { // Если ошибка
+            const errText = await response.text(); // Текст ошибки
             throw new Error(`Status: ${response.status} - ${errText}`);
         }
 
-        const result = await response.json();
-        TOKEN_buffer = await result.token;
-    } catch (err) {
-        print(`Error in fetch: ${err}`, "err");
-        return;
+        const result = await response.json(); // Результат запроса
+        TOKEN_buffer = await result.token; // Достать токен из результата
+    } catch (err) { // Если ошибка
+        print(`Error in fetch: ${err}`, "err"); // Вывод в консоль
+        return; // Выйти досрочно из ф-ции
     }
-    print(`TOKEN: ${TOKEN_buffer}`);
-    TOKEN = TOKEN_buffer;
+    print(`TOKEN: ${TOKEN_buffer}`); // Вывод токена в консоль
+    TOKEN = TOKEN_buffer; // Сохранение в глобальную переменную
+    // Записать токен в файл
     try {
-        const filePath = path.join(app.getPath("userData"), "token.txt");
+        const filePath = path.join(app.getPath("userData"), "token.txt"); // Путь к файлу
         fs.writeFileSync(filePath, TOKEN, "utf-8");
-        print("Token file was saved");
-    } catch (err) {
-        print(`Error in saving token file: ${err}`, "err");
-        return;
+        print("Token file was saved"); // Вывод в консоль
+    } catch (err) { // Если ошибка
+        print(`Error in saving token file: ${err}`, "err"); // Вывод в консоль
+        return; // Досрочно выйти из ф-ции
     }
+    // Уведомляю о успешном завершении ф-ции
     dialog.showMessageBox(win, {
         type: "info",
         title: "Info",
@@ -198,21 +204,28 @@ ipcMain.on("connect-api", async event => {
     })
 })
 
-ipcMain.on("connect-ws", connectWS);
+ipcMain.on("connect-ws", connectWS); // Ф-ция вынесена отдельно, потому что она так же может быть вызвана через автоматическое подключение
+                                     // при старте программы (если включено в настройках)
 
+// Подключение к ютуб музыке
 function connectWS() {
-    print("Connecting to WS server...");
+    print("Connecting to WS server..."); // Вывод в консоль
+    // Настройка сокета
     socket = io(SERVER_URL_WS, {
-        transports: ["websocket"],
+        transports: ["websocket"], // При помощи websocket
         auth: {
-            token: TOKEN
+            token: TOKEN // Токен
         }
     })
+    // При подключении сокета
     socket.on("connect", async () => {
-        print("Successfully connected");
-        win.webContents.send("ws-connected");
+        print("Successfully connected"); // Вывод в консоль
+        win.webContents.send("ws-connected"); // Отправка в electron
+        // Первый запрос делаю вручную, потому что если музыка стоит на паузе сокет даёт автоматически информацию
+        // гораздо реже
         try {
-            const url = "http://localhost:9863/api/v1/state";
+            // GET запрос
+            const url = "http://localhost:9863/api/v1/state"; // Адрес
             const response = await fetch(url, {
                 method: "GET",
                 headers: {
@@ -220,32 +233,41 @@ function connectWS() {
                     "Content-Type": "application/json"
                 }
             });
-            if (!response.ok) {
-                const errText = await response.text();
+            if (!response.ok) { // Если ошибка
+                const errText = await response.text(); // Текст ошибки
                 throw new Error(`Status: ${response.status} - ${errText}`);
             }
-            const state = await response.json();
-            win.webContents.send("state-update", state);
-        } catch (err) {
-            print(`Error in fetch: ${err}`, "err");
+            const state = await response.json(); // Распаковка результата
+            win.webContents.send("state-update", state); // Отправка в electron
+        } catch (err) { // Если ошибка
+            print(`Error in fetch: ${err}`, "err"); // Вывод в консоль
         }
     })
 
+    // Если у сокета ошибка -> Вывод в консоль -> Закрыть сокет
+    // Информирование об этом electron-а происходит в следующей ф-ции
     socket.on("connect_error", err => {
         print(`Error in Socket.io: ${err}`, "err");
         socket.disconnect();
     });
 
+    // Если сокет закрыт
     socket.on("disconnect", reason => {
-        print(`Socket.io closed, reason: ${reason}`);
+        print(`Socket.io closed, reason: ${reason}`); // Вывод в консоль
+        // Если окно electron еще существует -> Информирую его
         if (win && !win.isDestroyed()) win.webContents.send("ws-disconnected");
     });
 
-    socket.on("state-update", state => {
-        win.webContents.send("state-update", state);
-    })
+    // При каждом обновлении -> Отправка в electron
+    socket.on("state-update", state => win.webContents.send("state-update", state))
 }
 
+// Все ф-ции используют идентичную структуру:
+// 1) Вывод в консоль, что будет происходить
+// 2) Отправка соответствующего запроса через ф-цию sendPOST
+// 3) Если нужны данные, они передаются через переменную data
+
+// Изменить таймлайн
 ipcMain.on("change-timeline", async (event, data) => {
     print(`Change timeline to ${data}`);
     sendPOST("http://localhost:9863/api/v1/command", {
@@ -254,6 +276,7 @@ ipcMain.on("change-timeline", async (event, data) => {
     })
 })
 
+// Остановка \ Воспроизвидение
 ipcMain.on("play-pause", async () => {
     print("Play pause video");
     sendPOST("http://localhost:9863/api/v1/command", {
@@ -261,6 +284,7 @@ ipcMain.on("play-pause", async () => {
     })
 })
 
+// Переход к предыдушему треку
 ipcMain.on("prev", async () => {
     print("Previous video");
     sendPOST("http://localhost:9863/api/v1/command", {
@@ -268,6 +292,7 @@ ipcMain.on("prev", async () => {
     })
 })
 
+// Переход к следующему треку
 ipcMain.on("next", async () => {
     print("Next video");
     sendPOST("http://localhost:9863/api/v1/command", {
@@ -275,25 +300,7 @@ ipcMain.on("next", async () => {
     })
 })
 
-ipcMain.on("update-settings", (event, data) => {
-    print("Updating settings");
-    config = data;
-    try {
-        const filePath = path.join(app.getPath("userData"), "config.json");
-        fs.writeFileSync(filePath, JSON.stringify(data, null, 2), "utf-8");
-        print("Config file was saved");
-    } catch (err) {
-        print(`Error in saving config file: ${err}`, "err");
-        return;
-    }
-    dialog.showMessageBox(win, {
-        type: "info",
-        title: "Info",
-        message: "Settings were successfully updated and saved",
-        buttons: ["Close"]
-    })
-})
-
+// Изменение текущего трека, на заданный
 ipcMain.on("play-queue-index", (event, data) => {
     print(`Change video to ${data} (index in Queue)`);
     sendPOST("http://localhost:9863/api/v1/command", {
@@ -302,53 +309,85 @@ ipcMain.on("play-queue-index", (event, data) => {
     })
 })
 
+// Обновление конфига
+ipcMain.on("update-settings", (event, data) => {
+    print("Updating settings"); // Вывод в консоль
+    config = data; // Изменение переменной конфига на новую
+    try {
+        const filePath = path.join(app.getPath("userData"), "config.json"); // Путь к файлу
+        fs.writeFileSync(filePath, JSON.stringify(data, null, 2), "utf-8");
+        print("Config file was saved"); // Вывод результата
+    } catch (err) { // Если ошибка
+        print(`Error in saving config file: ${err}`, "err"); // Вывод в консоль
+        return; // Досрочно выйти из ф-ции
+    }
+    // Уведомляю пользователя
+    dialog.showMessageBox(win, {
+        type: "info",
+        title: "Info",
+        message: "Settings were successfully updated and saved",
+        buttons: ["Close"]
+    })
+})
+
+// Ф-ция перевода MM:SS.MS в секунды
 function strToNumLyr(str) {
-    const posDots = str.indexOf(':');
-    const min = Number(str.slice(0, posDots));
-    const sec = Math.round(Number(str.slice(posDots + 1)));
-    return min * 60 + sec;
+    const posDots = str.indexOf(':'); // Нахождения позиции [:]
+    const min = Number(str.slice(0, posDots)); // Число до :
+    const sec = Math.round(Number(str.slice(posDots + 1))); // Число после :
+                                                            // Миллисекунды окгрушляю (0-4 в меньшую, 5-9 в большую) для более точной синхронизации
+    return min * 60 + sec; // Возвращаю результат
 }
 
+// Запрос на текст песни
 ipcMain.on("require-lyrics", async (event, data) => {
-    print("Require lyrics");
-    const url = "https://lrclib.net/api/get";
+    print("Require lyrics"); // Вывод в консоль
+    const url = "https://lrclib.net/api/get"; // Адрес запроса
+    // Если сейчас ничего не играет -> досрочно выхожу из ф-ции
     if (data[0].length == 0 || data[1].length == 0 || data[2] == 0) {
         print("Require lyrics err. Nothing is playing");
         return;
     }
+    // Параметры для GET запроса
     const data_send = new URLSearchParams({
-        track_name: data[0],
-        artist_name: data[1],
-        duration: data[2]
+        track_name: data[0], // Название трека
+        artist_name: data[1], // Название исполнителя
+        duration: data[2] // Длина трека
     })
     try {
-        const response = await fetch(`${url}?${data_send}`);
-        if (!response.ok) {
-            const errText = await response.text();
+        const response = await fetch(`${url}?${data_send}`); // Формирую запроса
+        if (!response.ok) { // Если ошибка
+            const errText = await response.text(); // Текст ошибки
             throw new Error(`Status: ${response.status} - ${errText}`);
         }
-        const responseJson = await response.json();
-        let res;
-        let type;
-        if (responseJson.syncedLyrics == null) {
+        const responseJson = await response.json(); // Результат в json-е
+        let res; // Переменная для будущих слов
+        let type; // Тип будущих слов 
+        if (responseJson.syncedLyrics == null) { // Если в результате нет переменной с синхронизированными словами, то использовать обычные !добавить выбор!
             res = responseJson.plainLyrics.split('\n').map(element => ["plain", element]);
-            type = "plain";
-        } else {
-            res = responseJson.syncedLyrics.split('\n').map(element => {
-                const posOpen = element.indexOf("[") + 1;
-                const posClose = element.indexOf("]");
-                const time = strToNumLyr(element.slice(posOpen, posClose));
-                const lyr = element.slice(posClose + 2);
-                return [time, lyr];
+                        // Переменую делю по \n и меняю каждый елемент. Пример:
+                        // ["Текст1 \n Текст2 \n Текст3"] -> [["plain", "Текст1"], ["plain", "Текст2"], ["plain", "Текст3"]]
+            type = "plain"; // Задаю тип
+        } else { // Синхронизированные слова построчно
+            res = responseJson.syncedLyrics.split('\n').map(element => { // Переменую делю по \n и меняю каждый елемент
+                // Данные из syncedLyrics: "[00:17.12] I feel your breath upon my neck\n ... [MM:SS:MS] text"
+                const posOpen = element.indexOf("[") + 1; // Первая цифра находится по этому индексу (позиция скобки + 1)
+                const posClose = element.indexOf("]"); // Правая скобка находится по этому индексу
+                // Метод slice вырезает включительно с первым аргументом, но не включительно со вторым
+                const time = strToNumLyr(element.slice(posOpen, posClose)); // Передаю ф-ции которая вернет результат в секундах
+                const lyr = element.slice(posClose + 2); // Первая буква слов начинается Позиция ] + 2
+                                                         // Между словами и правой скобкой всегда стоит пробел
+                return [time, lyr]; // Результат записываеся таким образом
             });
-            type = "syn";
+            type = "syn"; // Задаю тип
         }
+        // Отправляю результат в electron
         win.webContents.send("lyrics-update", {
             lyr: res,
             type: type
         });
-        print("Lyrics were send to renderer");
-    } catch (err) {
-        print(`Error in fetch: ${err}`);
+        print("Lyrics were send to renderer"); // Вывожу результат в консоль
+    } catch (err) { // Если ошибка
+        print(`Error in fetch: ${err}`); // Вывод в консоль
     }
 })
